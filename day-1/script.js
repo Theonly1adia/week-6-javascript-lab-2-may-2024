68d4cfe9fdc077fb8ef75a0dc3b5986d10219f7b
Adia Holt <adia.holt@gmail.com>
2024-06-28 21:42:44 -0500

    update day 2

--- day-1/script.js
+++ day-1/script.js
@@ -96,13 +96,13 @@ function partThreeA () {
 // Try it!
 // This is a function that takes a number, x as an argument and returns the amount you shuold tip at 20%
 function twentyPercentTip(x) {
-  
+  return x*.20;
 }
 function partThreeB () {
   alert("Try it!");
   let bill, billNumber, tip, message;
   // 1. prompt the user for the bill
-  bill = prompt("WHat is your bill?");
+  bill = prompt("What is your bill?");
   // 2. convert the bill to a number
   billNumber = Number(bill);
   // 3. calculate the tip
@@ -142,6 +142,7 @@ function partFourA () {
 function splitBillByFour(x) {
   // Try it! return x divided by 4
   // to calculate the bill.
+  return x/4;
 }
 
 function partFourB () {
@@ -160,6 +161,10 @@ function partFourB () {
 
 
 // Bonus: Create a splitBillByN function that will take two arguements, the bill and n. N is the number of friends splitting.
+function splitBillByN(x) {
+  return bill/n;
+}
+
 // Then use the splitBillByN in part 4 A and B instead of splitBillByThree and splitBillByFour.
 // Think about how functions and arguments can make your code more flexible and ready to take on new requirements. (For example, can you make it split bill by 8 or 10 friends? How does splitBillByN help?)
 
