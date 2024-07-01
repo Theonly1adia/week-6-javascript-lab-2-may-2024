8aafae1937b19d1f14c4ceeb18f81257e1893a7c
Adia Holt <adia.holt@gmail.com>
2024-06-30 22:45:35 -0500

    update day 3

--- day-3/script.js
+++ day-3/script.js
@@ -21,9 +21,13 @@ function partOneB () {
   let friends 
   friends = [];
   // 1. TODO: Add your own friends to the array
-
+  friends[0] = "Adia";
+  friends[1] = "Brenda";
+  friends[2] = "Cecelia";
+  friends[3] = "Darryl";
+  friends[4] = "Enzo";
   // 2. TODO: write your friends to the message
-
+  messageParagraph.innerHTML = `Your friends are ${friends[0]}, ${friends[1]}, ${friends[2]}, ${friends[3]}, and ${friends[4]}.`;
 }
 
 // Example
@@ -51,27 +55,51 @@ function partTwoA () {
 function partTwoB () {
   let friends = [];
   // 1. Add your friends to the array
+  friends[0] = "Adia";
+  friends[1] = "Brenda";
+  friends[2] = "Cecelia";
+  friends[3] = "Darryl";
+  friends[4] = "Enzo";
 
   // 2. write your friends to the innerHTML of the document
-
+  messageParagraph.innerHTML = "Your friends are ";
+  messageParagraph.innerHTML += `${friends[0]}`;
+  messageParagraph.innerHTML += `, ${friends[1]}`;
+  messageParagraph.innerHTML += `, ${friends[2]}`;
+  messageParagraph.innerHTML += `, ${friends[3]}`;
+  messageParagraph.innerHTML += `, ${friends[4]}`;
 }
 
 // Example
 // add more friends from the prompt
 function partThreeA () {
   // 1. prompt for a friend
-  let friend = prompt("Enter a friend's name.");
+  let newFriends = prompt("Enter a friend's name.");
+
+  newFriends[0] = "Adia";
+  newFriends[1] = "Brenda";
+  newFriends[2] = "Cecelia";
+  newFriends[3] = "Darryl";
+  newFriends[4] = "Enzo";
 
   // 2. add the friend to the message
-  messageParagraph.innerHTML += `, ${friend}`;
+  let newfriends = prompt("Enter a friend's name.");
 }
 
 // Try it!
 // Try prompting for a friend with a slightly different prompt or writing the message in a slightly different format.
 function partThreeB () {
+  let friends = [];
   // 1. prompt for a friend
+  let friends = prompt("Enter a new friend's name.");
 
+  friends[0] = "Adia";
+  friends[1] = "Brenda";
+  friendsriends[2] = "Cecelia";
+  friendsriends[3] = "Darryl";
+  friendsriends[4] = "Enzo";
   // 2. add the friend to the message
+  let friends = prompt("Enter your new friend's name.");
 }
 
 // Example
--- day-4/script.js
+++ day-4/script.js
@@ -17,9 +17,11 @@ function partOneA () {
 function partOneB () {
   alert("Try it!");
   // 1. Create a for loop.
-
+  let highNumber = 7
     // 2. give the user and alert with the current number.
-
+  for (let i=0; i < highNumber; i++){
+    alert(`The number is ${i}`);
+  }
 }
 
 // Example
@@ -47,13 +49,15 @@ function partTwoB () {
   messageParagraph.innerHTML = "Flavors";
 
   // 1. Create an array of 5 flavors
-
+  let flavors = ["Chocolate", "Vanilla", "Strawberry", "Pecan", "Cherry Garcia"];
   // 2. Find the length of the array
-
+  let highNumber = flavors.length;
   // 3. Create a for loop.
-
-    // 4. give the user and alert with the current friend.
-
+  for (let i = 0; i < 6; i++) {
+  
+    // 4. give the user and alert with the current flavor.
+    alert (`The flavor is ${flavors[i]}`);
+  }
 }
 
 // Example
@@ -79,11 +83,13 @@ function partThreeB () {
 
   alert("Try it!");
   // 1. create an array of 5 flavors
-
+let flavors = ["Chocolate", "Vanilla", "Strawberry", "Pecan", "Cherry Garcia"];
   // 2. find the length of the array
- 
-  // 3. create a for loop
-
+  let numberOfFlavors = flavors.length;
+  // 3. create a for loop]
+  for (let i = 0; i < numberOfFlavors; i++){
+    messageList.innerHTML += `<li>${flavors[i]}</li>`;
+  }
     // 4. write the friend to the innerHTML of the list
 }
 
@@ -123,9 +129,9 @@ function partFourB () {
   let newFlavor, highNumber;
 
   // 1. prompt for a flavor
-
+  newFlavor = prompt("Add a flavor to the list.");
   // 2. add the flavor to the array with push()
-
+  flavors.push(newFlavor);
   // 3. reset the list in the innerHTML
   messageList.innerHTML = "";
   // 4. set the message to "Flavors"
@@ -135,6 +141,8 @@ function partFourB () {
   highNumber = flavors.length;
 
   // 6. write all the flavors back to the HTML list from the array, updated with our new flavor.
-
+  for (let i = 0; i < highNumber; i++) {
+    messageList.innerHTML += `<li>${flavors[i]}</li>`;
+  }
 }
 
